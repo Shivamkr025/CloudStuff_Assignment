51d74178416d19a541a42f2b3e581a67821d338c
Shivamkr025 <shivam22@navgurukul.org>
2024-07-18 10:44:05 +0530

    update server file

--- Backend/app.js
+++ Backend/app.js
@@ -3,6 +3,8 @@ import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import userRouter from './src/Router/userRouter.js';
+import projectRouter from './src/Router/projectRouter.js';
+import taskRouter from  './src/Router/taskRouter.js';
 
 dotenv.config();
 
@@ -13,7 +15,8 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use('/user', userRouter)
-
+app.use('/' , projectRouter)
+app.use('/' , taskRouter)
 
 const mongodb = process.env.Connect
 
