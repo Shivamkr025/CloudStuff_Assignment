9c5b2a4832fd8035dc9b90fcbc21be06438f9bb1
Shivamkr025 <shivam22@navgurukul.org>
2024-07-18 23:51:51 +0530

    add a validator

---
+++ Backend/src/Middleware/validator.js
@@ -0,0 +1,13 @@
+class Validator {
+    static validate(schema) {
+        return (req, res, next) => {
+            const { error } = schema.validate(req.body);
+            if (error) {
+                return res.status(400).json({ error: error.details[0].message });
+            }
+            next();
+        };
+    }
+}
+
+export default Validator;
